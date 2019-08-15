1f5ba4751e82ef329635bece47b3be3c277f53d8
Anton Österberg <anton.osterberg@gmail.com>
2019-08-15 12:27:06 +0200

    Testing out ideas. WIP

--- src/main/kotlin/Resulkt.kt
+++ src/main/kotlin/Resulkt.kt
@@ -8,15 +8,27 @@ package com.mantono.result
  *
  * 1. The operation was successful and an output of type [S] was returned
  * 2. An error was encountered, which may or may not have been caused by an
- * exception.
+ * cause.
  */
 sealed class Result<T> {
-	inline fun <S> onSuccess(operation: (T) -> Result<S>): Result<S> {
+	inline fun <S> tryThen(operation: (T) -> S): Result<S> {
 		return when(this) {
 			is Failure.Permanent -> Failure.Permanent(this)
 			is Failure.Transient -> Failure.Transient(this)
 			is Success -> try {
-				operation(this.value)
+				Success(operation(this.value))
+			} catch(e: Throwable) {
+				Failure.Permanent<S>(e.message ?: "Operation failed", e)
+			}
+		}
+	}
+
+	inline fun <S> then(operation: (T) -> S): Result<S> {
+		return when(this) {
+			is Failure.Permanent -> Failure.Permanent(this)
+			is Failure.Transient -> Failure.Transient(this)
+			is Success -> try {
+				Success(operation(this.value))
 			} catch(e: Throwable) {
 				Failure.fromException<S>(e)
 			}
@@ -27,17 +39,21 @@ sealed class Result<T> {
 		is Success -> this.value
 		is Failure -> throw ResultException(this)
 	}
+}
 
-	inline operator fun <S> invoke(operation: (T) -> Result<S>): Result<S> = onSuccess(operation)
+inline fun <S> tryExecute(operation: () -> S): Result<S> {
+	return try {
+		Success(operation())
+	} catch(e: Throwable) {
+		Failure.Permanent(e.message ?: "Operation failed", e)
+	}
+}
 
-	companion object {
-		inline operator fun <S> invoke(operation: () -> Result<S>): Result<S> {
-			return try {
-				operation()
-			} catch(e: Throwable) {
-				return Failure.fromException(e)
-			}
-		}
+inline fun <S> execute(operation: () -> S): Result<S> {
+	return try {
+		Success(operation())
+	} catch(e: Throwable) {
+		Failure.fromException(e)
 	}
 }
 
@@ -49,28 +65,30 @@ data class Success<T>(val value: T): Result<T>() {
 	override fun toString(): String = "Success [$value]"
 }
 
-sealed class Failure<T>: Result<T>() {
-	abstract val message: String
-	abstract val exception: Throwable?
-	abstract val metadata: Map<String, Any>
+interface TraceableError {
+	val message: String
+	val cause: Throwable?
+	val metadata: Map<String, Any>
+}
 
+sealed class Failure<T>: Result<T>(), TraceableError {
 	data class Permanent<T>(
 		override val message: String,
-		override val exception: Throwable? = null,
+		override val cause: Throwable? = null,
 		override val metadata: Map<String, Any> = emptyMap()
 	): Failure<T>() {
 		constructor(failure: Failure.Permanent<*>):
-			this(failure.message, failure.exception, failure.metadata)
+			this(failure.message, failure.cause, failure.metadata)
 	}
 
 	data class Transient<T>(
 		override val message: String,
-		override val exception: Throwable? = null,
+		override val cause: Throwable? = null,
 		override val metadata: Map<String, Any> = emptyMap()
 	): Failure<T>() {
 
 		constructor(failure: Failure.Transient<*>):
-			this(failure.message, failure.exception, failure.metadata)
+			this(failure.message, failure.cause, failure.metadata)
 	}
 
 	companion object {
@@ -86,8 +104,45 @@ sealed class Failure<T>: Result<T>() {
 
 class ResultException private constructor(
 	override val message: String,
-	override val cause: Throwable?
-): Exception(message, cause) {
-	internal constructor(result: Failure<*>): this(result.message, result.exception)
+	override val cause: Throwable? = null,
+	override val metadata: Map<String, Any> = emptyMap()
+): Exception(message, cause), TraceableError {
+	internal constructor(result: TraceableError): this(result.message, result.cause, result.metadata)
 	internal constructor(error: String): this(error, null)
+}
+
+internal class PermanentException(
+	override val message: String,
+	override val cause: Throwable? = null,
+	override val metadata: Map<String, Any> = emptyMap()
+): Throwable(), TraceableError {
+
+}
+
+
+fun failure(
+	message: String,
+	cause: Throwable? = null,
+	metadata: Map<String, Any?> = emptyMap()
+): Nothing {
+	val failure = Failure.Permanent(message)
+	val exception: Throwable = if(cause is TraceableError) {
+		ResultException(message, cause, cause.metadata + metadata)
+	} else {
+		PermanentException(message, cause, metadata)
+	}
+	throw exception
+}
+
+fun panic(
+	message: String,
+	cause: Throwable? = null,
+	metadata: Map<String, Any?> = emptyMap()
+): Nothing {
+	val exception: Throwable = if(cause is TraceableError) {
+		TransientException(message, cause, cause.metadata + metadata)
+	} else {
+		TransientException(message, cause, metadata)
+	}
+	throw exception
 }
